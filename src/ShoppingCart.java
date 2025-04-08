10ce4c25bd38464ca3502e7e79a0aaafe4ab374d
Alomgir Hasan Shakib <alomgirhasanshakib@gmail.com>
2025-04-08 16:17:31 +0600

    created a shopping cart program

---
+++ src/ShoppingCart.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ShoppingCart {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        String item;
+        double price;
+        int quentity;
+        char currency = '$';
+        double totalPrice = 0;
+
+        System.out.print("Which product would you like to buy? ");
+        item = scanner.nextLine();
+
+        System.out.print("How much price for Each one? ");
+        price = scanner.nextDouble();
+
+        System.out.print("How many would you like to buy? ");
+        quentity = scanner.nextInt();
+
+        totalPrice = price * quentity;
+
+        System.out.println("You have to pay total $" + totalPrice);
+
+
+
+        scanner.close();
+    }
+}
